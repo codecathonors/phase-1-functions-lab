dd2d91731378e5661dc473a7c261f6701f4872ad
CatherineHonors <cathonors@gmail.com>
2022-04-05 16:21:00 -0400

    commit tree 17d7c84c802bc40572fc3bfd38a49aace2bae568

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(amountAway) {
+    const headq = 42;
+    if (amountAway < 42) {
+        return 42 - amountAway
+    }
+    else if (amountAway > 42) {
+        return amountAway - 42
+    }
+}
+
+function distanceFromHqInFeet(amountAway) {
+    return distanceFromHqInBlocks(amountAway) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(destination - start) * 264)
+}
+
+function calculatesFarePrice (start, destination) {
+     let feet = distanceTravelledInFeet(start, destination);
+    if (feet < 400) {
+        return 0;
+    } else if (feet >= 400 && feet < 2000) {
+         return (feet - 400) * 0.02;
+     } else if (feet > 2000 && feet < 2500) {
+         return 25;
+     } else {
+        return "cannot travel that far";
+   }
+}
